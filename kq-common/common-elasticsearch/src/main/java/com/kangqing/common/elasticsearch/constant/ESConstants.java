e9d003fd727193ddf36abb69110cdccef7be6567
kangqing <kangqing.37@gmail.com>
2021-05-12 21:43:11 +0800

    完善通用ES包代码

---
+++ kq-common/common-elasticsearch/src/main/java/com/kangqing/common/elasticsearch/constant/ESConstants.java
@@ -0,0 +1,12 @@
+package com.kangqing.common.elasticsearch.constant;
+
+/**
+ *
+ * @author kangqing
+ * @since 2021/5/12 21:05
+ */
+public interface ESConstants {
+
+    int DEFAULT_PAGE_SIZE = 10;
+
+}
