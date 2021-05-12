f406cca37f18a4d13dd8b8dcd34bccc5981bdb2c
kangqing <kangqing.37@gmail.com>
2021-05-12 21:25:27 +0800

    完善通用mybatis包下的内容

---
+++ kq-common/common-mybatis/src/main/java/com/kangqing/common/mybatis/handler/FieldFillHandler.java
@@ -0,0 +1,31 @@
+package com.kangqing.common.mybatis.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 自动创建时间、更新时间处理器
+ *
+ * @author kangqing
+ * @since 2021/5/12 21:06
+ */
+@Component
+@Slf4j
+public class FieldFillHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+
+}
